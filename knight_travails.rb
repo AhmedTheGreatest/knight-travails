5ed83845389d001c819e195b1109c616d5edf668
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2024-01-09 19:36:07 +0500

    Add knight moves metd, and fixed bu bugs with the creation of the graph

--- knight_travails.rb
+++ knight_travails.rb
@@ -28,16 +28,61 @@ module KnightTravails
     end
 
     def build_graph
+      create_nodes
+      connect_nodes
+    end
+
+    def create_nodes
+      positions_to_create = Set.new
+      8.times do |x|
+        8.times do |y|
+          positions_to_create << [x, y]
+        end
+      end
+
+      positions_to_create.each do |position|
+        add_node(position)
+      end
+    end
+
+    def connect_nodes
       @nodes.each do |node|
         x, y = node.position
         VALID_MOVES.each do |transform_x, transform_y|
           new_x = x + transform_x
           new_y = y + transform_y
 
-          neighbor_node = find_node([new_x, new_y]) || add_node([new_x, new_y]) if valid_position?(new_x, new_y)
-          node.add_edge(neighbor_node)
+          if valid_position?(new_x, new_y)
+            neighbor_node = find_node([new_x, new_y])
+            node.add_edge(neighbor_node)
+          end
         end
       end
+    end
+
+    def knight_moves(start_pos, end_pos)
+      start_node = find_node(start_pos)
+      end_node = find_node(end_pos)
+
+      return nil unless start_node && end_node
+
+      visited = Set.new
+      queue = [[start_node.position]]
+
+      until queue.empty?
+        current_path = queue.shift
+        current_node = find_node(current_path.last)
+
+        return current_path if current_node == end_node
+
+        current_node.neighbors.each do |neighbor|
+          next_path = current_path + [neighbor.position]
+          queue << next_path unless visited.include?(neighbor.position)
+          visited << neighbor.position
+        end
+      end
+
+      nil
     end
 
     def add_node(position)
@@ -55,3 +100,6 @@ module KnightTravails
     end
   end
 end
+
+graph = KnightTravails::MovementGraph.new
+p graph.knight_moves([3, 3], [4, 3])
